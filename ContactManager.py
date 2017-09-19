42b2fd9b4a26ee719c316ed81b47db3a3789e5fe
Derrick Mwiti <mwitiderrick@gmail.com>
2017-09-19 17:07:05 +0000

    Added capability to read contacts from a file

--- ContactManager.py
+++ ContactManager.py
@@ -1,5 +1,6 @@
 # Contact manager with the ability to add, delete,search
 import sys
+import csv
 
 
 class Contact:
@@ -24,11 +25,25 @@ class Contact:
 class PhoneBook:
     def __init__(self, contacts=[]):
         self.contacts = contacts
+        line = 0
+        with open('contacts.csv', 'r') as contact_file:
+            for each_line in contact_file:
+                if line == 0:
+                    line += 1
+                    continue
+                # if len(each_line) > 1:
+                contact = each_line.strip('\n').split(',')
+                contact = {'Name': contact[0], 'Number': contact[1], 'Email': contact[2], 'Website': contact[3],
+                           'Birthday': contact[4]}
+                self.contacts.append(contact)
+
+        print(self.contacts)
+
 
     def delete_contact(self):
         contact_to_delete = input('Enter name of contact to delete: ')
         for contact in self.contacts:
-            if contact.name == contact_to_delete:
+            if contact['Name'] == contact_to_delete:
                 self.contacts.remove(contact)
         self.display_all_contacts()
 
@@ -38,7 +53,7 @@ class PhoneBook:
     def search_contact(self):
         contact_to_search = input('Enter name of the contact to search: ')
         for contact in self.contacts:
-            if contact.name == contact_to_search:
+            if contact['Name'] == contact_to_search:
                 print(contact)
         return 'Contact not found in phone book'
 
@@ -49,6 +64,7 @@ class PhoneBook:
 
 my_phone_book = PhoneBook()
 
+
 while True:
     print('Type 1 to Add new contact,2 to delete contact, 3 to search,contact, 4 to display all contact or q to exit: ')
     choice = input()
@@ -58,7 +74,8 @@ while True:
         email = input('Enter contacts email: ')
         website = input('Enter contacts website: ')
         birth_day = input('Enter contacts birthday: ')
-        my_phone_book.add_contact(Contact(name=name, number=number, email=email, website=website, birth_day=birth_day))
+        my_phone_book.add_contact({'Name': name, 'Number': number, 'Email': email, 'Website': website,
+                           'Birthday': birth_day})
         for a_contact in my_phone_book.contacts:
             print(a_contact)
     elif choice == '2':
@@ -68,6 +85,17 @@ while True:
     elif choice == '4':
         my_phone_book.display_all_contacts()
     elif choice == 'q':
-        sys.exit()
+        with open('contacts.csv', 'w') as contact_file:
+            contact_file.write('Name,Number,Email,Website,Birthday\n')
+            for contact in my_phone_book.contacts:
+                print(contact)
+                name = contact['Name']
+                number = contact['Number']
+                email = contact['Email']
+                website = contact['Website']
+                birth_day = contact['Birthday']
+                comma_separated_contacts = name+','+number+','+email+','+website+','+birth_day+'\n'
+                contact_file.write(comma_separated_contacts)
+            sys.exit()
     else:
         print('Please choose the correct input')
